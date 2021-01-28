a7b94deb1a67f37125b7385a70f4866277b35424
benhtodd <benhtodd@gmail.com>
2021-01-28 11:32:46 -0800

    updates

---
+++ klog.bat
@@ -0,0 +1,3 @@
+kubectl vsphere login --server wcp.lab.livefire.dev --vsphere-username %1 --insecure-skip-tls-verify
+
+kubectl vsphere login --server wcp.lab.livefire.dev --vsphere-username toddb@lab.livefire.dev --insecure-skip-tls-verify  --tanzu-kubernetes-cluster-name toddb-prom  --tanzu-kubernetes-cluster-namespace monitoring
